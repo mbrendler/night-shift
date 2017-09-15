d2cf18f4b778e109146aaaf2a96e3c2e7418a79b
Maik Brendler <maik.brendler@injixo.com>
2017-09-15 19:47:12 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+set -ex
+
+CC="${CC:-cc}"
+
+PRIVATE_FRAMEWORKS_DIR=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk/System/Library/PrivateFrameworks
+
+"${CC}" \
+  -Wall \
+  -Wpedantic \
+  -o night-shift \
+  -F"$PRIVATE_FRAMEWORKS_DIR" \
+  -framework Foundation \
+  -framework CoreBrightness \
+  night-shift.m
