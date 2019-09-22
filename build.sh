7e05e9c7010b317904b8354b2c0c0eabbb05cf3c
Maik Brendler <maik.brendler@injixo.com>
2019-09-22 12:41:10 +0200

    Allow to build with debug information

--- build.sh
+++ build.sh
@@ -6,9 +6,16 @@ CC="${CC:-cc}"
 
 PRIVATE_FRAMEWORKS_DIR=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/PrivateFrameworks
 
+BUILD_OPTIONS="-O3"
+
+if [ "$1" = '--debug' ] ; then
+  BUILD_OPTIONS="-g"
+fi
+
 "${CC}" \
   -Wall \
   -Wpedantic \
+  "$BUILD_OPTIONS" \
   -o night-shift \
   -F"$PRIVATE_FRAMEWORKS_DIR" \
   -framework Foundation \
